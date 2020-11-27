d546c3f28ceeed75e041bf2933b9eb13557243aa
Sangwan Han <jklom0326@gmail.com>
2020-11-28 01:05:46 +0900

    commit

---
+++ Sorting/두배열의원소교체.py
@@ -0,0 +1,16 @@
+n, k = map(int, input().split())
+a =list(map(int, input().split()))
+b =list(map(int, input().split()))
+
+a.sort() # 배열 A는 오름차순으로 정렬 수행
+b.sort(reverse= True) # 배열 B는 내림차순으로 정렬 수행
+
+# 첫 번째 인덱스부터 확인하며, 두 배열의 원소를 최대 K번 비교
+for i in range(k):
+    # A의 원소가 B의 원소보다 작은 경우
+    if a[i] < b[i]:
+        # 두 원소를 교체
+        a[i], b[i] = b[i], a[i]
+    else:
+        break
+print(sum(a))
